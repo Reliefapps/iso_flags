fffbf274a427753fe2a83e4ac3cbb69ac7ddc836
Raphael <raphael@reliefapplications.org>
2017-03-15 21:03:38 +0100

    Update iso2toiso3.php

--- iso2toiso3.php
+++ iso2toiso3.php
@@ -1,13 +1,13 @@
 <?php
 
-$json_string = '/Users/raphael_b/Desktop/countryISO.json';
+$json_string = '~/Desktop/countryISO.json';
 
 $jsondata = file_get_contents($json_string);
 $obj = json_decode($jsondata, true);
 print_r($obj);
 
 foreach ($obj as $iso2 => $iso3){
-  shell_exec('cp /Users/raphael_b/Desktop/48_iso2/"'.$iso2.'" /Users/raphael_b/Desktop/48_iso3/"'.$iso3.'"');
+  shell_exec('cp ~/Desktop/48_iso2/"'.$iso2.'" ~/Desktop/48_iso3/"'.$iso3.'"');
 }
 
 
